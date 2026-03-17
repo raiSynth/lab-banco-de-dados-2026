03ba073688d48c9931f194ae368489158e956cb8
RaiSynth <raissakoj333@gmail.com>
2026-03-16 21:06:15 -0300

    Delete AULA1 directory

--- AULA1/criar_tabela_livro.sql
+++
@@ -1,9 +0,0 @@
- CREATE table livro(
-	id_livro INT AUTO_INCREMENT PRIMARY KEY,
-    tituo VARCHAR(255) NOT NULL,
-    sinopse TEXT,
-    autor VARCHAR(128) 	NOT NULL,
-    editora VARCHAR(128) NOT NULL,
-    categoria VARCHAR(128) 	NOT NULL,
-    ano_publicacao YEAR
- )
